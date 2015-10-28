8c6e56de32d08e49e1ad36876051534f7bab8874
mrmmathias <dix_mccoy@yahoo.com>
2015-10-28 11:32:13 +0200

    ajax and bootstrap

--- controllers/scripts.js
+++ controllers/scripts.js
@@ -8,10 +8,35 @@ $(document).ready(function(){
     $(".about").html("<b>koivari</b>");
     $("[data-dummy]").html("<p>owpworpo</p>");
     
+    
     /*var data = document.getElementsByTagName("nav");
     for (i=0; <data.length i++){
         data[i].style.backgroundColor = "lightblue"
     }*/
+    
+    var setting = {
+    
+        method:"GET",
+        url:"http://localhost:28017/oma/person/",
+        dataType:"jsonp",
+        jsonp:"jsonp"
+    }
+    
+    $.ajax(setting).done(function(data){
+       
+        console.log(data);
+        
+        for(i=0; i < data.rows.length; i++){
+            
+            var html = "<tr>" +
+                        "<td>" +data.rows[i].name + "</td>" +
+                        "<td>" +data.rows[i].address + "</td>" +
+                        "<td>" +data.rows[i].age + "</td>" +
+                        "</tr>";
+            $(html).appendTo("tbody");
+        }
+    });
+    
 });
 /*
 window.onload = function(event){
